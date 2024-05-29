589ed5a2c231cbdc1dfd69a1a16c428f922dae37
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-05-30 00:32:00 +0530

    Applying board logic

--- project.js
+++ project.js
@@ -6,6 +6,21 @@
 //6.give the user their money
 //7.play again
 const prompt = require("prompt-sync")();
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOL_COUNTS = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+const SYMBOL_VALUES = {
+    "A":5   ,
+    "B":4,
+    "C":3,
+    "D":2
+}
 const deposit = () =>{
     while(true){
         const depositAmount = prompt("Enter the amount to deposit : ");
@@ -42,8 +57,52 @@ const getBet = (balance,lines) =>{
             return BetAmt;
         }
     }
-}
+};
+const spin = () => {
+    const symbols = [];
+    for(const [symbol,count] of Object.entries(SYMBOL_COUNTS)){
+        for(let i = 0;i<count;i++){
+            symbols.push(symbol);
+        }
+    }
+    const reels = [];
+    for(let i = 0 ;i<COLS;i++){ 
+        reels.push([])
+        const reelSymbols= [...symbols];
+        for(let j = 0;j<ROWS;j++){
+            const RandomIndex = Math.floor(Math.random()*reelSymbols.length);
+            const selectedSymbols = reelSymbols[RandomIndex];
+            reels[i].push(selectedSymbols);
+            reelSymbols.splice(RandomIndex,1);
+        }
+    }
+    return reels
+};
+const transpose = (reels) =>{
+    const rows = [];
+    for(let i = 0;i<ROWS;i++){
+        rows.push([])
+        for(let j = 0;j<COLS;j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+};
+const printrows = (rows) => {
+    for(const row of rows){
+        let rowString = " ";
+        for(const[i,symbol] of row.entries()){
+            rowString += symbol;
+            if(i!=row.length -1 ){
+                rowString += " | "
+            }
+        }
+        console.log(rowString)
+    }
+};
 let balance = deposit();
 const Lines = NumberofLinestoBetOn();
 const BetAmount = getBet(balance,Lines);
-console.log(BetAmount);
+const reels = spin();
+const rows = transpose(reels);
+printrows(rows);
